ed2cd5ab940ca003b1cde6b18675a662005049b9
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-27 18:18:19 -0400

    create Movie schema and model

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/movieApp");
   console.log("Connection success!");
 }
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+const parasite = new Movie({
+  title: "Parasite",
+  year: 2019,
+  score: 99,
+  rating: "R",
+});
