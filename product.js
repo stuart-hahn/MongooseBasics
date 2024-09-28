c855514dec527a03f801d2926b5eac8d285acf58
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-28 14:43:08 -0400

    feat: add Product model with type validations

---
+++ product.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+main().catch((err) => console.error(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/shopApp");
+  console.log("Connection success!");
+}
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+const pizzaPan = new Product({
+  name: "Pizza Pan",
+  price: "18.99",
+});
+
+pizzaPan
+  .save()
+  .then((data) => {
+    console.log("It worked.");
+    console.log(data);
+  })
+  .catch((err) => {
+    "It panicked!";
+    console.error(err);
+  });
+
+console.log(pizzaPan);
